2e81b4a7eab9377180e6386551828f682b85f278
rayyagenaro <gennarorayya05@gmail.com>
2023-11-30 15:09:21 +0700

    Pertemuan 30 Nop 2023

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<br/>
+<br/>
+<hr/>
+<footer>
+    <p>&copy; Rayya Genaro Iswandoko 2023 </p>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,94 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ("welcome");
+    return view('welcome');
 });
 
-Route::get('blog', function () {
-    return view ("blog");
+Route::get('/tugas-1', function () {
+    return view('tugas1') ;
+});
+
+Route::get('/tugas-2', function () {
+    return view('tugas2') ;
+});
+
+Route::get('/tugas-3', function () {
+    return view('tugas3') ;
+});
+
+Route::get('/tugas-4', function () {
+    return view('tugas4') ;
+});
+
+Route::get('/tugas-5', function () {
+    return view('tugas5') ;
+});
+
+Route::get('/tugas-6', function () {
+    return view('tugas6') ;
+});
+
+Route::get('/blog2', function () {
+    return view('blog') ;
 });
 
 Route::get('/blog', function () {
-    return view('home') ;
+    return view('home');
 });
 
 Route::get('/blog/tentang', function () {
-    return view('tentang') ;
+    return view('tentang');
 });
 
 Route::get('/blog/kontak', function () {
-    return view('kontak') ;
+    return view('kontak');
 });
 
-Route::get('meet1', function () {
-    return view("meet1");
-});
+Route::get('/perkalian', 'App\Http\Controllers\DosenController@index');
 
-Route::get('meet2', function () {
-    return view("meet2");
-});
+Route::get('/biodata', 'App\Http\Controllers\DosenController@biodata');
 
-Route::get('meet3', function () {
-    return view("meet3");
-});
+Route::get('/showjam/{jam}', 'App\Http\Controllers\DosenController@showtime');
 
-Route::get('meet3-1', function () {
-    return view("meet3-1");
-});
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
 
-Route::get('meet3-2', function () {
-    return view("meet3-2");
-});
-
-Route::get('meet3-3', function () {
-    return view("meet3-3");
-});
-
-Route::get('meet3-4', function () {
-    return view("meet3-4");
-});
-
-Route::get('meet4', function () {
-    return view("meet4");
-});
-
-Route::get('meet5-1', function () {
-    return view("meet5-1");
-});
-
-Route::get('meet5-2', function () {
-    return view("meet5-2");
-});
-
-Route::get('meet6', function () {
-    return view("meet6");
-});
-
-Route::get('meet7', function () {
-    return view("meet7");
-});
-
-Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
-
-Route::get('perkalian', 'ccccDosenController@index');
-Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showtime');
-Route::get('formulir', 'App\Http\Controllers\DosenController@formulir');
 Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
-
-
 
 //route CRUD
 Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+
 Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+
 Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+
 Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+
 Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+
 Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
 
-
-
-
-
-
-
-
+Route::get('/pegawai/cari','App\Http\Controllers\PegawaiController@cari');
